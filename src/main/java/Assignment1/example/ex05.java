451179377be0db54d87ae9b25b5be3771d3bde98
polvnco <cpolanco000@gmail.com>
2021-06-03 22:12:26 -0400

    Finished

--- src/main/java/Assignment1/example/ex05.java
+++ src/main/java/Assignment1/example/ex05.java
@@ -5,10 +5,33 @@
 package Assignment1.example;
 
 
+import java.util.Scanner;
+
 public class ex05
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.print("What is the first number? ");
+        Scanner firstNum = new Scanner(System.in);
+        String x = firstNum.nextLine();
+        System.out.print("What is the second number? ");
+        Scanner secondNum = new Scanner(System.in);
+        String y = secondNum.nextLine();
+
+        int a = Integer.parseInt(x);
+        int b = Integer.parseInt(y);
+        int c = a + b;
+        int d = a - b;
+        int e = a * b;
+        int f = a / b;
+
+
+
+
+        System.out.print(
+                a + " " + "+" + " "  + b + " "  + "=" + " "  + c + "\n" + a + " " + "-" + " "  + b + " "  + "=" + " "  + d + "\n" +
+                a + " " + "*" + " "  + b + " "  + "=" + " "  + e + "\n" + a + " " + "/" + " "  + b + " "  + "=" + " "  + f );
+
+
     }
 }
