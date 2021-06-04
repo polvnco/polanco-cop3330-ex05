6d018a747706f69e40fbbb6ffd462162393391a2
polvnco <cpolanco000@gmail.com>
2021-06-03 21:37:31 -0400

    Me learning how to use Git with IntelliJ

---
+++ src/main/java/Assignment1/example/ex05.java
@@ -0,0 +1,14 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Christopher Polanco
+ */
+package Assignment1.example;
+
+
+public class ex05
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
